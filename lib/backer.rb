b66e3fcbb1b32de93adbfba8bcc89753476b47e4
Bihelca Quinonez <bquinonez@my.uri.edu>
2019-10-16 03:45:15 +0000

    Automatically backed up by Learn

--- lib/backer.rb
+++ lib/backer.rb
@@ -1,10 +1,12 @@
 class Backer
-  attr_reader :name
+  attr_reader :name, :backed_projects
   
-  @backed_projects = []
+  
   
   def initialize(name)
     @name = name
-    
   end
+  
+  
+  
 end
